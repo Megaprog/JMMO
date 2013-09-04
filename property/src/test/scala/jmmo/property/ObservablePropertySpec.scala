2866eb3b8bafeac70ba604f5c6295bbfb938d1d8
Tomas Shestakov <shestakovte@gmail.com>
2013-09-04 16:19:06 +0400

    added parent pom.xml added ObservablePropertySpec and PropertySuite

---
+++ property/src/test/scala/jmmo/property/ObservablePropertySpec.scala
@@ -0,0 +1,14 @@
+package jmmo.property
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.mock.MockitoSugar
+
+/**
+ * User: Tomas
+ * Date: 04.09.13
+ * Time: 15:12
+ */
+class ObservablePropertySpec extends WordSpec with ShouldMatchers with MockitoSugar {
+
+}
---
+++ property/src/test/scala/jmmo/property/PropertySuite.scala
@@ -0,0 +1,21 @@
+package jmmo.property
+
+import org.scalatest.WordSpec
+
+/**
+ * User: Tomas
+ * Date: 04.09.13
+ * Time: 15:13
+ */
+class PropertySuite extends WordSpec {
+
+  override val nestedSuites = List(
+    new ObservablePropertySpec
+  )
+
+  "Observables" should {
+
+    "fit all specifications in package" in {}
+
+  }
+}
