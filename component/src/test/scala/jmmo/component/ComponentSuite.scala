40a57bfa6edb4c358c96bc82e91b91c4e3cdc69e
Tomas Shestakov <shestakovte@gmail.com>
2013-09-19 13:57:37 +0400

    added ComponentSuite

---
+++ component/src/test/scala/jmmo/component/ComponentSuite.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2013 Tomas Shestakov. <https://github.com/Megaprog/JMMO>
+ */
+
+package jmmo.component
+
+import org.scalatest.WordSpec
+import jmmo.observable.impl.ListenerWrapperSpec
+
+/**
+ * User: Tomas
+ * Date: 03.08.13
+ * Time: 20:34
+ */
+class ComponentSuite extends WordSpec {
+
+  override val nestedSuites = List(
+    new ComponentSpec
+  )
+
+  "Components" should {
+
+    "fit all specifications in package" in {}
+
+  }
+}
